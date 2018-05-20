f2262fa1f00d1dcdcb1ac0333607db7ccb8ca6d4
Alexandra DeWit <alexa.dewit@gmail.com>
2018-05-20 15:31:12 +0200

    Scaffolding some stuff and thinking about ideas

--- src/DoGame.hs
+++ src/DoGame.hs
@@ -3,17 +3,21 @@ module DoGame
   , Sub (..)
   ) where
 
+import qualified DoGame.Cmd as Cmd
+
 -- Event subscriptions for the DoGame runtime to listen to.
 -- The DoGame runtime will also subscribe to events generated from its own internal Cmd system.
 -- One need not set up runtime subs, as Cmds which set up eventing loops will set up their own subs
 data Sub a
-  = Sub
+  = Sub a
+
+newtype Scene a = Scene a
 
 -- The configuration for a game, namely the functions needed to power the runtime for your game
 data Game m a
   = Game
-    { initConst     :: (m, Cmd a)
-    , updateFn      :: a -> m -> (m, Cmd a)
-    , viewFn        :: m -> Collage a
+    { initConst     :: (m, Cmd.Cmd m a)
+    , updateFn      :: a -> m -> (m, Cmd.Cmd m a)
+    , viewFn        :: m -> Scene a
     , subscriptions :: Sub a
     }
--- src/DoGame/Cmd.hs
+++ src/DoGame/Cmd.hs
@@ -1,9 +1,15 @@
-module Cmd
+module DoGame.Cmd
   ( Cmd(..)
+  , none
   ) where
 
 -- Commands that can be sent to the underlying DoGame runtime
 data Cmd m a
   = ArbitraryIO (m -> IO a)
   | EnableTextEntry (String -> a)
+  | NoOp
+
+
+none :: Cmd m a
+none = NoOp
 
